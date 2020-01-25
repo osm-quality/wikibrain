0938e8db671c8301f899c650086edfcad5153c4a
Mateusz Konieczny <matkoniecz@gmail.com>
2020-01-25 15:29:53 +0100

    fix dependencies

--- setup.py
+++ setup.py
@@ -13,6 +13,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/matkoniecz/wikibrain",
     packages=setuptools.find_packages(),
+    install_requires = [
+        'geopy>=1.11.0',
+        'nose>=1.3.7',
+    ],
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
