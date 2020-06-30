9d446cbcfc6fb21e282f69acb29fb15e135b2fc1
Mateusz Konieczny <matkoniecz@gmail.com>
2020-07-01 00:08:42 +0200

    add failing test for reported bug
    thanks to Markus for reporting

--- test_wikimedia_link_issue_reporter.py
+++ test_wikimedia_link_issue_reporter.py
@@ -165,5 +165,10 @@ class Tests(unittest.TestCase):
         wikidata = self.issue_reporter().get_effective_wikidata_tag(tags)
         self.assertEqual("Q52412", wikidata)
 
+    def test_576_in_future(self):
+        wikimedia_connection.set_cache_location(osm_handling_config.get_wikimedia_connection_cache_location())
+        self.assertEqual(self.issue_reporter().check_is_object_is_existing('Q650270'), None)
+        
+
 if __name__ == '__main__':
     unittest.main()
