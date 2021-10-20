fdceb60d208d9d3bff6acc8eefcb93978fbfa8d8
Mateusz Konieczny <matkoniecz@gmail.com>
2021-10-20 16:23:52 +0200

    flushed

--- flush.py
+++ flush.py
@@ -3,8 +3,7 @@ import osm_handling_config.global_config as osm_handling_config
 import os
 
 wikimedia_connection.set_cache_location(osm_handling_config.get_wikimedia_connection_cache_location())
-kill = ["Q334166", "Q811683"
-]
+kill = [] #NOT FLUSHED YET!
 for id in kill:
     os.remove(wikimedia_connection.get_filename_with_wikidata_entity_by_id(id))
     os.remove(wikimedia_connection.get_filename_with_wikidata_by_id_response_code(id))
