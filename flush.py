374dbc9ebadf14bb4691d9730265c9dfb4497994
Mateusz Konieczny <matkoniecz@gmail.com>
2021-10-20 16:32:23 +0200

    better flush docs

--- flush.py
+++ flush.py
@@ -3,7 +3,11 @@ import osm_handling_config.global_config as osm_handling_config
 import os
 
 wikimedia_connection.set_cache_location(osm_handling_config.get_wikimedia_connection_cache_location())
-kill = [] #NOT FLUSHED YET!
+kill = ["Q45621"] #NOT FLUSHED YET!
+"""
+# use like this:
+kill = ["Q45621"]
+"""
 for id in kill:
     os.remove(wikimedia_connection.get_filename_with_wikidata_entity_by_id(id))
     os.remove(wikimedia_connection.get_filename_with_wikidata_by_id_response_code(id))
