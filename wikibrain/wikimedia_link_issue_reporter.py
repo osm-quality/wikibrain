8f713a03768d30d4994f8f84c6b3026043542867
Mateusz Konieczny <matkoniecz@gmail.com>
2023-01-05 07:59:18 +0100

    one more workaround

--- wikibrain/wikimedia_link_issue_reporter.py
+++ wikibrain/wikimedia_link_issue_reporter.py
@@ -1332,6 +1332,9 @@ class WikimediaLinkIssueDetector:
         wikidata_bugs.append('Q1263068') # duplicate database entry - self-report of Wikidata ontology bug
         wikidata_bugs.append('Q17362920') # Wikimedia duplicated page - self-report of Wikidata ontology bug
 
+        # isolated network is not an event
+        wikidata_bugs.append("Q1664456")
+
         # ferry routes are not events
         # https://www.wikidata.org/w/index.php?title=Wikidata_talk:WikiProject_Ontology&oldid=1782894312#Tramwaj_wodny_w_Bydgoszczy_(Q926453)_is_an_event,_according_to_Wikidata_ontology 
         wikidata_bugs.append("Q18984099")
