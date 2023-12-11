2cbc37e95d87f2015998ac399bba5960e323be07
Mateusz Konieczny <matkoniecz@gmail.com>
2023-12-11 13:19:37 +0100

    shared reinstall

--- reinstall.sh
+++ reinstall.sh
@@ -8,10 +8,9 @@ trap 'err_report $LINENO' ERR
 
 rm dist -rf
 python3 setup.py sdist bdist_wheel
-cd dist
-pip3 uninstall wikibrain -y
-pip3 install --user *.whl
-cd ..
+
+# better as library?
+python3 ../../python_package_reinstaller/reinstaller.py wikibrain
 
 # synch with README
 pylint **/*.py --include-naming-hint=y --variable-rgx="^[a-z][a-z0-9]*((_[a-z0-9]+)*)?$" --disable=C0103,C0301,C0114,C0115,C0116,C0121,W0612,W0613,R0904,R0911,R0912,R0913,R0914,R0915,C0302,C1803,R1710,W0719,R1705,C0411 || true
